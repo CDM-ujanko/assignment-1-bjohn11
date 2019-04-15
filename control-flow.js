518ad7acd51bdf323381cb7e15cd9d3934ff3e35
Brian John <Brian@loop-248-185.loop-wireless.depaul.edu>
2019-04-15 17:26:38 -0500

    part two

---
+++ control-flow.js
@@ -0,0 +1,22 @@
+if (false) {
+// code
+    console.log('1 is smaller than 0!')
+} else if(true) {
+    console.log('1 is equal to 0')
+} else{
+    console.log('else');
+}
+
+
+var smth = "soMething";
+if(smth == "Something" && (smth == "something" || smth == "something")) {
+    console.log('They are equal');
+}
+
+
+var statment = 1 === 1 && 2 === 2 || 3 === 34;
+
+if (statment){
+    console.log('My statement is true!');
+}
+
---
+++ data-type.js
@@ -0,0 +1,80 @@
+console.log('Hello')
+console.log($)
+
+var myNewVar = 5;
+
+console.log(myNewVar);
+
+let mySecondVar = 7;
+
+console.log(mySecondVar);
+ 
+console.log((myNewVar + mySecondVar) * 2);
+
+let myFloat = 2.2;
+
+console.log(myFloat);
+
+var myString= 'Hello world';
+
+console.log(myString + ' Some more text');
+
+
+var theTrue = true;
+console.log(theTrue);
+
+
+let myEmptyVar;
+console.log(myEmptyVar);
+
+
+var myArr= ['House','Cow',2, false, 'seven'];
+console.log(myArr, myArr[1]);
+
+var myOBj = {animal1: 'cow',
+             object: 'House'
+            };
+console.log(myOBj)
+
+
+var student = {
+    
+id:00000,
+age: 31,
+name: 'bob',
+year: 2,
+year: 2,
+};
+console.log(student);
+console.log(student.age);
+
+
+function myAdditionfunc(arg1, arg2, arg3) {
+    return arg1 + arg2 + arg3;
+}
+
+function myDumbFunction(){
+    console.log('Hello');
+}
+
+myArr[3] = true;
+
+var shoppingList = [];
+console.log(shoppingList);
+
+shoppingList.push('Milk');
+shoppingList.push('Eggs');
+shoppingList.push('Pizza', 'Beer');
+
+
+shoppingList.pop();
+console.log(shoppingList.pop());
+
+shoppingList.unshift('one','two');
+shoppingList.shift();
+
+shoppingList.push([1,2]);
+shoppingList.push(student)
+
+
+
